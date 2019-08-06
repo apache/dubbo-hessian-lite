1c88173f719bc68c30baa92464d255580bca43d5
wanglian <lljava@163.com>
2019-08-06 17:59:31 +0800

    支持枚举类中包含其他变量
    支持枚举类中包含其他变量，枚举中如果包含其他变量，目前会丢失

--- src/main/java/com/alibaba/com/caucho/hessian/io/EnumSerializer.java
+++ src/main/java/com/alibaba/com/caucho/hessian/io/EnumSerializer.java
@@ -48,14 +48,22 @@
 
 package com.alibaba.com.caucho.hessian.io;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Serializing an object for known object types.
  */
 public class EnumSerializer extends AbstractSerializer {
     private Method _name;
+
+    private HashMap<String, Method> _methodMap;
 
     public EnumSerializer(Class cl) {
         // hessian/32b[12], hessian/3ab[23]
@@ -67,9 +75,10 @@ public class EnumSerializer extends AbstractSerializer {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
+        _methodMap = getMethodMap(cl);
     }
 
-    @Override
     public void writeObject(Object obj, AbstractHessianOutput out)
             throws IOException {
         if (out.addRef(obj))
@@ -87,20 +96,147 @@ public class EnumSerializer extends AbstractSerializer {
             throw new RuntimeException(e);
         }
 
+        JSONObject jo = new JSONObject();
+        wlDecode(obj, jo, _methodMap);
+        jo.put("_", name);
+        String value = jo.toJSONString();
+
+        // System.out.println("EnumSerializer writeObject " + value);
+
         int ref = out.writeObjectBegin(cl.getName());
 
         if (ref < -1) {
-            out.writeString("name");
-            out.writeString(name);
+            out.writeString("enum");
+            out.writeString(value);
             out.writeMapEnd();
         } else {
             if (ref == -1) {
                 out.writeClassFieldLength(1);
-                out.writeString("name");
+                out.writeString("enum");
                 out.writeObjectBegin(cl.getName());
             }
 
-            out.writeString(name);
+            out.writeString(value);
+
         }
+    }
+
+    private void wlDecode(Object obj, JSONObject jo, HashMap<String, Method> methodMap) {
+        for (Map.Entry<String, Method> me : methodMap.entrySet()) {
+            Class c = me.getValue().getReturnType();
+            if (Enum.class.isAssignableFrom(c)) {
+                try {
+                    Object _obj = me.getValue().invoke(obj);
+                    if (_obj == null) continue;
+
+                    JSONObject _jo = new JSONObject();
+
+                    Method _method = c.getMethod("name", new Class[0]);
+                    String _name = (String) _method.invoke(_obj, (Object[]) null);
+                    _jo.put("_", _name);
+
+                    HashMap<String, Method> _methodMap = getMethodMap(c);
+
+                    wlDecode(_obj, _jo, _methodMap);
+
+                    jo.put(me.getKey(), _jo);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                try {
+                    jo.put(me.getKey(), me.getValue().invoke(obj));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates a map of the classes fields.
+     */
+    protected HashMap<String, Method> getMethodMap(Class cl) {
+        HashMap<String, Method> methodMap = new HashMap<String, Method>();
+
+        for (; cl != null; cl = cl.getSuperclass()) {
+            Method[] methods = cl.getDeclaredMethods();
+
+            for (int i = 0; i < methods.length; i++) {
+                Method method = methods[i];
+
+                if (Modifier.isStatic(method.getModifiers()))
+                    continue;
+
+                if (method.getParameterTypes().length != 0)
+                    continue;
+
+                String name = method.getName();
+
+                if (!name.startsWith("get"))
+                    continue;
+
+                Class type = method.getReturnType();
+
+                if (type.equals(void.class))
+                    continue;
+
+                if (findGetter(methods, name, type) == null)
+                    continue;
+
+                // XXX: could parameterize the handler to only deal with public
+                try {
+                    method.setAccessible(true);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+
+                name = name.substring(3);
+
+                int j = 0;
+                for (; j < name.length() && Character.isUpperCase(name.charAt(j)); j++) {
+                }
+
+                if (j == 1)
+                    name = name.substring(0, j).toLowerCase() + name.substring(j);
+                else if (j > 1)
+                    name = name.substring(0, j - 1).toLowerCase() + name.substring(j - 1);
+
+
+                methodMap.put(name, method);
+            }
+        }
+
+        methodMap.remove("declaringClass");
+        methodMap.remove("class");
+
+        return methodMap;
+    }
+
+    /**
+     * Finds any matching setter.
+     */
+    private Method findGetter(Method[] methods, String setterName, Class arg) {
+        String getterName = "get" + setterName.substring(3);
+
+        for (int i = 0; i < methods.length; i++) {
+            Method method = methods[i];
+
+            if (!method.getName().equals(getterName))
+                continue;
+
+            if (!method.getReturnType().equals(arg))
+                continue;
+
+            Class[] params = method.getParameterTypes();
+
+            if (params.length == 0)
+                return method;
+        }
+
+        return null;
     }
 }
